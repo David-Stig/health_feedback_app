aa510820495b81a84c144d0278f031a8f4cb9644
chis7 <54583653+chis7@users.noreply.github.com>
2026-04-18 01:23:42 +0200

    Update settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -79,8 +79,8 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": os.getenv("POSTGRES_DB", "health_feedback"),
-        "USER": os.getenv("POSTGRES_USER", "postgres"),
-        "PASSWORD": os.getenv("POSTGRES_PASSWORD", "p@ssw0rd"),
+        "USER": os.getenv("POSTGRES_USER", "hfuser"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD", "1234"),
         "HOST": os.getenv("POSTGRES_HOST", "localhost"),
         "PORT": os.getenv("POSTGRES_PORT", "5432"),
     }
