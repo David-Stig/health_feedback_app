2780efa8f212e5438bfb0cf1191221c0f64d2b8a
chis7 <54583653+chis7@users.noreply.github.com>
2026-04-19 02:43:14 +0200

    Update settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -117,9 +117,9 @@ MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
 
-LOGIN_URL = "login"
-LOGIN_REDIRECT_URL = "dashboard:home"
-LOGOUT_REDIRECT_URL = "login"
+LOGIN_REDIRECT_URL = '/dashboard/'
+LOGOUT_REDIRECT_URL = '/'
+LOGIN_URL = '/'
 
 
 CACHES = {
